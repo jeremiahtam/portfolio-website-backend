f7015f1a5d155dbe9ad400703eafcb695ef8c938
Esite Jeremiah Tamarankuroemi <33421069+jeremiahtam@users.noreply.github.com>
2024-08-18 23:42:51 +0100

    Check if fileLocation was found

--- src/routes/filesRoute.ts
+++ src/routes/filesRoute.ts
@@ -5,7 +5,11 @@ const router = Router();
 
 router.get('/fetch-image/:file(*)', (req, res) => {
   let fileName = req.params.file;
-  let fileLocation = path.resolve(__dirname, '../storage/projectImages/', fileName);;
-  res.sendFile(`${fileLocation}`)
+  let fileLocation = path.resolve(__dirname, '../storage/projectImages/', fileName);
+  if (fileLocation) {
+    res.status(200).sendFile(`${fileLocation}`)
+  } else {
+    res.status(404).send({ message: 'Could not find image' })
+  }
 })
 export default router;
