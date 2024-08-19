d95a66543b26233305524edf670e69394449e1ec
Esite Jeremiah Tamarankuroemi <33421069+jeremiahtam@users.noreply.github.com>
2024-08-19 01:03:35 +0100

    Removed filesRoutes Made storage folder a static directory Renamed images in the storage folder

--- createApp.ts
+++ createApp.ts
@@ -2,23 +2,25 @@ import express from "express"
 import projectsRouter from './src/routes/projectsRoute'
 import contactMeRouter from './src/routes/contactMeRoute'
 import userRouter from './src/routes/usersRoute'
-import filesRouter from './src/routes/filesRoute'
 import authRouter from './src/routes/authRoute'
 import { authWithPassport } from "./src/middleware/authentication"
 
 import cookieParser from "cookie-parser";
 import session from "express-session";
 import passport from "passport";
-import mongoose from 'mongoose'
 import MongoStore from "connect-mongo";
 import cors from 'cors'
 import fileUpload from 'express-fileupload'
-import serverless from "serverless-http";
+import path from 'path'
 
 export const createApp = () => {
   let app = express();
   // default options
   app.use(fileUpload());
+
+  // Serve the 'storage' folder as a static directory
+  app.use(express.static(path.join(__dirname, 'src/storage')));
+  app.use('/image', express.static('src/storage/projectImages'))
 
   /** cors Setup*/
   const origin1 = process.env.CORS_ORIGIN_URL_1 as string
@@ -62,7 +64,6 @@ export const createApp = () => {
   /** All routers to be used */
   app.use(projectsRouter)
   app.use(contactMeRouter)
-  app.use(filesRouter)
   app.use(userRouter)
   app.use(authRouter)
 
